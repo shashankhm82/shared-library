b0172a0ff8c572f6dde77f81d67b6c251d9b6a86
shashankhm82 <102578492+shashankhm82@users.noreply.github.com>
2022-12-14 22:03:35 +0530

    Update sendofUser.groovy

--- vars/sendofUser.groovy
+++ vars/sendofUser.groovy
@@ -1,1 +1,18 @@
-
+def call (String username = 'null', String designation = 'null'){
+    echo "this is shared library greetUser: ${username}"
+    pipeline {
+        environment {
+            USER_NAME = "${username}"
+          DESG = "${designation}"
+        }
+        agent any
+        stages {
+            stage('SHARED STAGE'){
+                steps{
+                    echo "hi $USER_NAME, how are you!"
+                    echo "Designation: $DESG"
+                }
+            }
+        }
+    }
+}
